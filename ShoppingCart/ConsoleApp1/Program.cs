00e45cd80cd87f046d89702e6902826fa550c1e9
Ajaykrishnan U <ajaykrishnan0391@gmail.com>
2018-09-24 14:32:09 +0530

    Installed Entity framework and added a console app

---
+++ ShoppingCart/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ModelDBContext obj = new ModelDBContext();
+            var data = obj.Products.ToList();
+            var a = 10;
+        }
+    }
+}
